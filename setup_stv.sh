5be131eca7b1a3289b11d5a0f2206571d87bcb15
Steven Gardiner <gardiner@fnal.gov>
2024-09-23 14:33:32 -0500

    Update MCC9.1 uboonecode version that gets set up by setup_stv.sh

--- setup_stv.sh
+++ setup_stv.sh
@@ -2,7 +2,7 @@
 
 # Sets up the local environment for working with the STV analysis scripts
 source /cvmfs/uboone.opensciencegrid.org/products/setup_uboone.sh
-setup uboonecode v08_00_00_52 -q e17:prof
+setup uboonecode v08_00_00_84 -q e17:prof
 
 # Finds the directory where this script is located. This method isn't
 # foolproof. See https://stackoverflow.com/a/246128/4081973 if you need
