c6f07eaa6c61f2c1960a5458a9ced1de040bb39f
Patrick <pgreen135@github.com>
2025-04-09 15:45:16 +0100

    cleaning up and disabling NuMI configuration mode by default

--- setup_xsec_analyzer.sh
+++ setup_xsec_analyzer.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # Get the name of the OS release (used to distinguish between SL7 and EL9)
-#MY_OS_REL=$(cat /etc/os-release | grep ^NAME | sed -e 's/NAME=//g' -e 's/"//g')
+MY_OS_REL=$(cat /etc/os-release | grep ^NAME | sed -e 's/NAME=//g' -e 's/"//g')
 
 # Sets up the local environment for working with xsec_analyzer
-#if [ "$MY_OS_REL" = "AlmaLinux" ]; then
+if [ "$MY_OS_REL" = "AlmaLinux" ]; then
   # On AL9, we set up ROOT and a recent compiler version
-#  source /cvmfs/larsoft.opensciencegrid.org/spack-v0.22.0-fermi/setup-env.sh
-#  spack load gcc@12.2.0 arch=linux-almalinux9-x86_64_v3
-#  spack load root@6.28.12 arch=linux-almalinux9-x86_64_v3
-#elif [ "$MY_OS_REL" = "Scientific Linux" ]; then
+  source /cvmfs/larsoft.opensciencegrid.org/spack-v0.22.0-fermi/setup-env.sh
+  spack load gcc@12.2.0 arch=linux-almalinux9-x86_64_v3
+  spack load root@6.28.12 arch=linux-almalinux9-x86_64_v3
+elif [ "$MY_OS_REL" = "Scientific Linux" ]; then
   # On SL7, we get ROOT as a side-effect of setting up uboonecode
-#  source /cvmfs/uboone.opensciencegrid.org/products/setup_uboone.sh
-#  setup uboonecode v08_00_00_84 -q e17:prof
-#else
-#  echo "WARNING: Unrecognized OS name \"${MY_OS_REL}\""
-#  echo "Unable to automatically set up ROOT"
-#fi
+  source /cvmfs/uboone.opensciencegrid.org/products/setup_uboone.sh
+  setup uboonecode v08_00_00_84 -q e17:prof
+else
+  echo "WARNING: Unrecognized OS name \"${MY_OS_REL}\""
+  echo "Unable to automatically set up ROOT"
+fi
 
 # Finds the directory where this script is located. This method isn't
 # foolproof. See https://stackoverflow.com/a/246128/4081973 if you need
@@ -26,8 +26,6 @@ THIS_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export XSEC_ANALYZER_DIR=${THIS_DIRECTORY}
 export PATH=${PATH}:${XSEC_ANALYZER_DIR}/bin
-
-echo "$(uname)"
 
 # Set the library path for loading the XSecAnalyzer shared library at runtime
 if [ "$(uname)" = "Darwin" ]; then
